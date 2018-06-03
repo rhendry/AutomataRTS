1a35fb7d9e81022eb59edeb724c3cae3d329c38b
roberthendry <robert.i.hendry@gmail.com>
2018-06-03 14:43:31 -0400

    Added a vehicle from the asset store and turnt the wheels.

--- Assets/Scripts/Movement/LandVehicleMover.cs
+++ Assets/Scripts/Movement/LandVehicleMover.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class LandVehicleMover : MonoBehaviour {
 
+    public float EffectiveSpeed { get; private set; }
+
     /// <summary>
     /// Speed in m/s.
     /// </summary>
-    public double speed = 10;
+    public float Speed = 1;
 
     /// <summary>
     /// Current heading.
     /// </summary>
-    public Vector2 heading = Vector2.zero;
+    public Vector3 Heading = Vector3.zero;
 
     private Rigidbody rigidbody_;
-    private Transform transform_;
 
 	// Use this for initialization
 	void Start () {
         rigidbody_ = GetComponent<Rigidbody>();
-        transform_ = GetComponent<Transform>();
 	}
-	
+
+    private float change = .5f;
+    private float t = 0;
+
 	// Update is called once per frame
 	void FixedUpdate () {
-        //rigidbody_.AddForce(new Vector3(1f, 0, 0) * Time.deltaTime);
-        //System.Console.WriteLine(rigidbody_.velocity);
-        //transform_.position = rigidbody_.velocity;
-        rigidbody_.MovePosition(transform_.position + transform_.forward * Time.deltaTime);
+
+        if (t > change) {
+            //Vector3 newHeading = new Vector3(Random.value, 0, Random.value); //x, y, z - y is height, z is axis towards default camera
+            //transform.forward = Vector3.RotateTowards(transform.forward, newHeading, Vector3.SignedAngle(transform.forward, newHeading, Vector3.up), Mathf.PI);
+            EffectiveSpeed = 2 * Random.value * Speed;
+            t = 0;
+        }
+        t += Time.deltaTime;
+        rigidbody_.MovePosition(transform.position + EffectiveSpeed * transform.forward * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/Movement/WheelRotator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotator : MonoBehaviour {
+
+    public string FrontRightName;
+    public string FrontLeftName;
+    public string BackRightName;
+    public string BackLeftName;
+
+    private LandVehicleMover mover_;
+    private Transform frontRight_;
+    private Transform frontLeft_;
+    private Transform backRight_;
+    private Transform backLeft_;
+
+    private float circumference_ = 1;
+
+    // Use this for initialization
+    void Start() {
+        mover_ = GetComponent<LandVehicleMover>();
+        frontRight_ = transform.Find(FrontRightName);
+        backLeft_ = transform.Find(BackLeftName);
+        backRight_ = transform.Find(BackRightName);
+        frontLeft_ = transform.Find(FrontLeftName);
+
+        // Assuming all the wheels are the same size, get the approximate radius
+        var mesh = frontRight_.gameObject.GetComponent<Renderer>().bounds.size;
+        circumference_ = 2 * Mathf.PI * mesh.y/2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // Get distance vehicle has traveled since last update
+        float d = mover_.EffectiveSpeed * Time.deltaTime;
+
+        // Get the fraction of rotations (in degrees) to cover distance d
+        float rotations = 360 * d / circumference_;
+
+        // Rotate wheels based on vehicle speed
+        if (frontRight_ != null) {
+            frontRight_.Rotate(Vector3.right, rotations);
+        }
+        if (frontLeft_ != null) {
+            frontLeft_.Rotate(Vector3.right, rotations);
+        }
+        if (backRight_ != null) {
+            backRight_.Rotate(Vector3.right, rotations);
+        }
+        if (backLeft_ != null) {
+            backLeft_.Rotate(Vector3.right, rotations);
+        }
+
+    }
+}
