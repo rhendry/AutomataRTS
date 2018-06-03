92d1989e875de56ffc6df36eec15f9a3ea62b443
Rob <robert.i.hendry@gmail.com>
2018-06-03 09:07:02 -0400

    Initial commit.

---
+++ Assets/Scripts/Movement/LandVehicleMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandVehicleMover : MonoBehaviour {
+
+    /// <summary>
+    /// Speed in m/s.
+    /// </summary>
+    public double speed = 10;
+
+    /// <summary>
+    /// Current heading.
+    /// </summary>
+    public Vector2 heading = Vector2.zero;
+
+    private Rigidbody rigidbody_;
+    private Transform transform_;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody_ = GetComponent<Rigidbody>();
+        transform_ = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //rigidbody_.AddForce(new Vector3(1f, 0, 0) * Time.deltaTime);
+        //System.Console.WriteLine(rigidbody_.velocity);
+        //transform_.position = rigidbody_.velocity;
+        rigidbody_.MovePosition(transform_.position + transform_.forward * Time.deltaTime);
+	}
+}
